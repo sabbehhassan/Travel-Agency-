bcab7a6f5b52c4f886f88c8772cf7e31836cabc1
sabbehhassan <hassansubbeh2446@gmail.com>
2025-10-22 23:05:51 +0500

    edit scrolltotop

--- tour-to-heaven/src/routes/appRoutes.jsx
+++ tour-to-heaven/src/routes/appRoutes.jsx
@@ -7,7 +7,7 @@ import Login from "../pages/Login/index.jsx";
 import Register from "../pages/Register/index.jsx";
 import UserDashboard from "../pages/UserDash/index.jsx";
 import ProfilePage from "../pages/Profile/index.jsx";
-import ScrollToTop from "../components/scrolltotop/index.jsx";
+import ScrollToTop from "../components/ScrollToTop/index.jsx";
 import CustomizeTrip from "../pages/customizetrip/index.jsx";
 import AdminDashboard from "../pages/adminDash/index.jsx";
 import Destination from "../pages/destination/index.jsx";
